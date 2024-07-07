408c98444f4768b003d357887a8b9e666a24d0db
user <user@localhost>
2024-07-07 17:04:11 -0600

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,6 +27,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
 
   deploymentBranch:'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
